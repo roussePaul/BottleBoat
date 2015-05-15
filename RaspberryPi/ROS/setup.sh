f440fdf322453a8f445184a3a54a950528ae4561
roussePaul <rousse@kth.se>
2015-05-15 06:25:25 +0000

    compass setup update

--- RaspberryPi/ROS/setup.sh
+++ RaspberryPi/ROS/setup.sh
@@ -3,5 +3,5 @@
 basedir=$(pwd -L)
 # Use "pwd -P" for the path without links. man bash for more info.
 
-export QUICK2WIRE_API_HOME=$basedir/src/compass/include/compass/quick2wire-python-api
+export QUICK2WIRE_API_HOME=$basedir/src/compass/src/quick2wire-python-api
 export PYTHONPATH=$PYTHONPATH:$QUICK2WIRE_API_HOME
