3440f5f0f284918c0d29f9ffdc901d8647d07491
roussePaul <rousse@kth.se>
2015-05-15 07:33:33 +0000

    Compass working

--- RaspberryPi/ROS/src/compass/scripts/get_heading.py
+++ RaspberryPi/ROS/src/compass/scripts/get_heading.py
@@ -1,28 +1,27 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # license removed for brevity
 
-from i2clibraries import i2c_hmc5883l
+import hmc5883l
 import rospy
 from std_msgs.msg import Float32
 
-def compass():
+def handle_compass():
 
 	pub = rospy.Publisher('heading', Float32, queue_size=10)
 	rospy.init_node('compass', anonymous=True)
-	rate = rospy.Rate(10) # 10hz
+	rate = rospy.Rate(3) # 3hz
 
-	hmc5883l = i2c_hmc5883l.i2c_hmc5883l(1)
-	hmc5883l.setContinuousMode()
-	hmc5883l.setDeclination(9,54)
+	compass = hmc5883l.hmc5883l(gauss = 4.7, declination = (-2,5))
+
 	rospy.loginfo("Compass ready.")
 	
 	while not rospy.is_shutdown():
-		(degress, minutes) = hmc5883l.getHeading()
-		pub.publish(degress)
+		degress = compass.degrees(compass.heading())
+		pub.publish(degress[0])
 		rate.sleep()
 
 if __name__ == '__main__':
 	try:
-		compass()
+		handle_compass()
 	except rospy.ROSInterruptException:
 		pass
